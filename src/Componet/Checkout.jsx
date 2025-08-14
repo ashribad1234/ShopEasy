f9678b6fa403c9a1b76ba4ae720010ce27bb658d
aprusty <aprusty@pixentia.com>
2025-08-14 22:38:36 +0530

    latestchanges

--- src/Componet/Checkout.jsx
+++ src/Componet/Checkout.jsx
@@ -57,7 +57,7 @@ const Checkout = () => {
                         <div className="input-group">
                             <input type="text" className="form-control" placeholder="Promo code" />
                             <div className="input-group-append">
-                                <button type="submit" className="btn btn-secondary">Redeem</button>
+                                <button type="submit" className="btn btn-secondary">Redeehm</button>
                             </div>
                         </div>
                     </form>
